2cb71f0cef536f53b06a7f7acf7638eb4fbfd478
Matej Kafka <git@a-n.cc>
2022-04-24 17:51:01 +0300

    Delete obsolete _random_scripts dir

--- app/Pog/_random_scripts/add_version_dirs.ps1
+++
@@ -1,9 +0,0 @@
-Import-Module $PSScriptRoot"\..\Paths"
-
-ls -Directory $MANIFEST_REPO | % {
-	$m = ls $_ -Recurse -Include manifest.psd1 | Import-PowerShellDataFile | select Name, Version
-	echo $m
-	$items = ls $_
-	$d = ni -Type Directory "$_\$($m.Version)"
-	$items | % {Move-Item $_ $d}
-}
