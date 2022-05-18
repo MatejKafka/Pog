cb1878b635868638d6aefbf2edf83fd62acc29b7
Matej Kafka <git@a-n.cc>
2022-05-18 23:03:12 +0300

    VersionParser: Move to a class to make usage easier

--- app/Pog/lib/VersionParser.Tests.ps1
+++ app/Pog/lib/VersionParser.Tests.ps1
@@ -1,42 +1,49 @@
 # Requires -Version 7
+using module .\VersionParser.psm1 # to use the [DevVersionType] enum
+
 BeforeAll {
-	$module = Import-Module -Force $PSScriptRoot\VersionParser -PassThru
+	$Module = Import-Module -Force $PSScriptRoot\VersionParser -PassThru
+}
+
+AfterAll {
+	Remove-Module $Module
+	Remove-Variable Module
 }
 
 Describe "Version parsing" {
 	BeforeAll {
-		$function:ParseVersion = & $module {$function:ParseVersion}
+		function TestArrayExact($a, $b) {
+			$a | Should -Be $b
+			# for some reason, PowerShell thinks that the local and returned [DevVersionType]
+			#  are a different type, so we have to compare type names instead
+			$a | % {$_.GetType().Name} | Should -Be ($b | % {$_.GetType().Name})
+		}
 	}
 
-	AfterAll {
-		Remove-Item Function:ParseVersion
-	}
-	
 	It "parses PowerShell rc versions correctly" {
-		$p = ParseVersion "7.1.0-rc5"
-		$p.Main | Should -Be @(7, 1, 0)
-		$p.Dev | Should -Be @("rc", 5)
-		$p.Dev | % {$_.GetType()} | Should -Be @([string], [int])
+		$p = New-PackageVersion "7.1.0-rc5"
+		TestArrayExact $p.Main @(7, 1, 0)
+		TestArrayExact $p.Dev @([DevVersionType]::Rc, 5)
 	}
 	
-	It "parses Firefox versions correctly" {
-		$p = ParseVersion "78.0a2"
-		$p.Main | Should -Be @(78, 0)
-		$p.Dev | Should -Be @("a", 2)
-		$p.Dev | % {$_.GetType()} | Should -Be @([string], [int])
+	It "parses Firefox development versions correctly" {
+		$p = New-PackageVersion "89.0a1-2021-04-05"
+		TestArrayExact $p.Main @(89, 0)
+		TestArrayExact $p.Dev @([DevVersionType]::Alpha, 1, 2021, 4, 5)
 	}
 	
 	It "parses pypy versions correctly" {
-		$p = ParseVersion "3.6-v3.7.1"
-		$p.Main | Should -Be @(3, 6)
-		$p.Dev | Should -Be @("v", 3, ".", 7, ".", 1)
-		$p.Dev | % {$_.GetType()} | Should -Be @([string], [int], [string], [int], [string], [int])	
+		$p = New-PackageVersion "3.6-v3.7.1"
+		TestArrayExact $p.Main @(3, 6)
+		TestArrayExact $p.Dev @("v", 3, 7, 1)
 	}
 }
 
 Describe "Version comparison" {
 	BeforeAll {
-		$function:IsVersionGreater = & $module {$function:IsVersionGreater}
+		function IsVersionGreater($V1, $V2) {
+			return (New-PackageVersion $V1) -gt (New-PackageVersion $V2)
+		}
 	}
 
 	AfterAll {
@@ -48,6 +55,8 @@ Describe "Version comparison" {
 		IsVersionGreater "1.4" "1.4.1" | Should -BeFalse
 		IsVersionGreater "1.4.1-beta5" "1.4.1" | Should -BeFalse
 		IsVersionGreater "1.4-beta2.1" "1.4-beta2" | Should -BeTrue
+		# should be equal
+		(New-PackageVersion "1.0").CompareTo((New-PackageVersion "1")) | Should -Be 0
 	}
 
 	It "correctly compares JetBrains style versions" {
@@ -61,6 +70,7 @@ Describe "Version comparison" {
 		IsVersionGreater "7.1.1" "7.1.0rc5" | Should -BeTrue
 		# rc5 is earlier than release 7.1.0 version
 		IsVersionGreater "7.1.0" "7.1.0rc5" | Should -BeTrue
+		IsVersionGreater "7.1.0rc1" "7.1.0rc5" | Should -BeFalse
 		IsVersionGreater "5.1.0" "7.0.0" | Should -BeFalse
 		IsVersionGreater "1.2.0rc2" "7.1.0rc1" | Should -BeFalse
 		IsVersionGreater "7.1.0rc2" "7.1.0rc1" | Should -BeTrue
@@ -76,11 +86,7 @@ Describe "Version comparison" {
 	It "correctly compares pypy versions" {
 		IsVersionGreater "3.6-v3.7.1" "3.6-v4.0.0" | Should -BeFalse
 		IsVersionGreater "3.6-v3.7.1" "3.6-v3.7.2" | Should -BeFalse
-		
-		# technically, this should be true, but we cannot reliably expand
-		#  the comparison to multiple dash-separated groups without breaking
-		#  on some dev suffixes
-		IsVersionGreater "3.6-v3.7.1" "3.6-v3.7.1-b1" | Should -BeFalse
+		IsVersionGreater "3.6-v3.7.1" "3.6-v3.7.1-b1" | Should -BeTrue
 	}
 
 	It "correctly compares 7zip versions" {
--- app/Pog/lib/VersionParser.psm1
+++ app/Pog/lib/VersionParser.psm1
@@ -18,57 +18,141 @@ function Zip($a1, $a2, $filler = $null) {
 	}
 }
 
-function ParseVersion($V) {
-	$VReg = "^(?<Main>\d[\.\d]+)-?(?<Dev>.*)$"
-	if ($V -notmatch $VReg) {
-		throw "Could not parse package version: " + $V
-	}
-	
-	$ParsedMain = $Matches.Main.Split(".") | ? {$_} | % {[int]::Parse($_)}
-	
-	$ParsedDev = @()
-	$cType = $null
-	$c = ""
-	$Matches.Dev.ToCharArray() | % {
-		if ([char]::IsDigit($_) -eq $cType) {
-			$c += $_
-		} else {
-			if ($c -ne "") {$ParsedDev += if ($cType) {[int]::Parse($c)} else {"" + $c}}
-			$cType = [char]::IsDigit($_)
-			$c = $_
+enum DevVersionType {
+	Nightly = 0
+	Preview = 1
+	Alpha = 2
+	Beta = 3
+	Rc = 4
+}
+$DevVersionTypeMap = @{
+	nightly = [DevVersionType]::Nightly
+	preview = [DevVersionType]::Preview
+	alpha = [DevVersionType]::Alpha
+	a = [DevVersionType]::Alpha
+	beta = [DevVersionType]::Beta
+	b = [DevVersionType]::Beta
+	rc = [DevVersionType]::Rc
+}
+
+# https://github.com/PowerShell/PowerShell/issues/10669
+<# Used to parse and represent package versions. There's quite a lot of heuristics here,
+   but it behaves sanely for all the version formats I encountered yet. #>
+class PogPackageVersion : System.IComparable, System.IEquatable[Object] {
+	<# Main part of the version – dot-separated numbers, not necessarily semver. #>
+	[int[]]$Main
+	<# Development version suffix – "beta.1", "preview-1.2.3",... #>
+	[array]$Dev
+	<# The original unchanged version string. #>
+	[string]$VersionString
+
+	PogPackageVersion([string]$VersionString) {
+		$this.VersionString = $VersionString
+
+		$VReg = "^(?<Main>\d[\.\d]*)(?<Dev>.*)$"
+		if ($VersionString -notmatch $VReg) {
+			throw "Could not parse package version: " + $VersionString
 		}
+
+		$this.Main = $Matches.Main.Split(".") | ? {$_} | % {[int]::Parse($_)}
+
+		$this.Dev = @()
+		$IsNumericToken = $null
+		[string]$Token = ""
+		function Flush {
+			if ($Token -eq "") {return}
+			$this.Dev += if ($IsNumericToken) {[int]::Parse($Token)}
+				elseif ($DevVersionTypeMap.ContainsKey($Token)) {$DevVersionTypeMap[$Token]}
+				else {[string]$Token}
+		}
+		$Matches.Dev.ToCharArray() | % {
+			if ($_ -in ".", "-", "_") {
+				# split on these chars
+				Flush
+				$IsNumericToken = $null
+				$Token = ""
+			} elseif ([char]::IsDigit($_) -eq $IsNumericToken) {
+				$Token += $_
+			} else {
+				Flush
+				$IsNumericToken = [char]::IsDigit($_)
+				$Token = $_
+			}
+		}
+		Flush
 	}
-	if ($c -ne "") {$ParsedDev += if ($cType) {[int]::Parse($c)} else {"" + $c}}
-	
-	return @{
-		Main = $ParsedMain
-		Dev = $ParsedDev
+
+	[string] ToString() {
+		return $this.VersionString
+	}
+
+	[int] CompareTo($Version2) {
+		$V1 = $this
+		$V2 = [PogPackageVersion]$Version2
+		# compare the main (semi-semver) part
+		foreach ($_ in (Zip $V1.Main $V2.Main)) {
+			# if one of the versions is shorter than the other, treat the extra fields as zeros
+			$P1 = if ($null -eq $_[0]) {0} else {$_[0]}
+			$P2 = if ($null -eq $_[1]) {0} else {$_[1]}
+			if ($P1 -eq $P2) {continue}
+			return $P1.CompareTo($P2)
+		}
+
+		# same semi-semvers, no dev suffix, the versions are equal
+		if ($V1.Dev.Count -eq 0 -and $V2.Dev.Count -eq 0) {return 0}
+		# V2 is dev version of V1 -> V1 is greater
+		if ($V1.Dev.Count -eq 0) {return 1}
+		# V1 is dev version of V2 -> V2 is greater
+		if ($V2.Dev.Count -eq 0) {return -1}
+
+		# both have dev suffix and the same semi-semver, compare dev suffixes
+		foreach ($_ in (Zip $V1.Dev $V2.Dev)) {
+			# here's the fun part, because the dev suffixes are quite free-style
+			# each possible version field type has an internal ordering
+			# if both fields have a different type, the following priorities are used:
+			#  string < DevVersionType < null < int, where later values are considered greater than earlier ones
+			#  effectively:
+			#   - $null is treated as -1 (int)
+			#   - DevVersionType vs string – could be basically anything, assume that DevVersionType is newer
+			#   - DevVersionType vs int – assume that int is a newer version ("almost-release"?)
+			#   - int vs string – assume that int is a newer version ("almost-release", there are no qualifiers)
+
+			$P1 = if ($null -eq $_[0]) {-1} else {$_[0]}
+			$P2 = if ($null -eq $_[1]) {-1} else {$_[1]}
+
+			if ($P1 -eq $P2) {continue}
+
+			$TypeOrder = @{[string] = 0; [DevVersionType] = 1; [int] = 2}
+			$O1 = $TypeOrder[$P1.GetType()]
+			$O2 = $TypeOrder[$P2.GetType()]
+			if ($O1 -ne $O2) {
+				# different field types, compare based on the field type ordering above
+				return $O1.CompareTo($O2)
+			} else {
+				# same field types, use the default comparator for the type
+				return $P1.CompareTo($P2)
+			}
+		}
+		# both Main and Dev parts are equal
+		return 0
+	}
+
+	[bool] Equals($Version2) {
+		if ($Version2 -isnot [PogPackageVersion]) {
+			return $false
+		}
+		return $this.CompareTo($Version2) -eq 0
 	}
 }
 
-function IsVersionGreater($V1, $V2) {
-	$P1 = ParseVersion $V1
-	$P2 = ParseVersion $V2
-
-	# compare semver part
-	foreach ($_ in (Zip $P1.Main $P2.Main)) {
-		if ($_[0] -eq $_[1]) {continue}
-		return $_[0] -gt $_[1]
-	}
-	
-	# same semvers, no dev suffix
-	if ($P1.Dev.Count -eq 0 -and $P2.Dev.Count -eq 0) {return $false}
-	# V2 is dev version of V1 -> V1 is greater
-	if ($P1.Dev.Count -eq 0) {return $true}
-	# V1 is dev version of V2 -> V2 is greater
-	if ($P2.Dev.Count -eq 0) {return $false}
-	
-	# both have dev suffix and same semver, compare dev suffix
-	foreach ($_ in (Zip $P1.Dev $P2.Dev)) {
-		if ($_[0] -eq $_[1]) {continue}
-		return $_[0] -gt $_[1]
-	}
-	return $false
+Export function New-PackageVersion {
+	[OutputType([PogPackageVersion])]
+	param(
+			[Parameter(Mandatory)]
+			[string]
+		$VersionString
+	)
+	return [PogPackageVersion]::new($VersionString)
 }
 
 Export function Get-LatestVersion {
@@ -81,12 +165,13 @@ Export function Get-LatestVersion {
 	if ($Versions.Count -eq 0) {
 		throw "Passed version list is empty."
 	}
-	
-	$Max = $Versions[0]
-	$Versions | select -Skip 1 | % {
-		if (IsVersionGreater $_ $Max) {
+
+	$ParsedVersions = $Versions | % {[PogPackageVersion]$_}
+	$Max = $ParsedVersions[0]
+	$ParsedVersions | select -Skip 1 | % {
+		if ($_ -gt $Max) {
 			$Max = $_
 		}
 	}
-	return $Max
+	return $Max.ToString()
 }
