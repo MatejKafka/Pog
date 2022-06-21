d51dc42cfe6e9b2ae22fce4a1a1518c8608c1985
Matej Kafka <git@a-n.cc>
2022-06-21 20:20:45 +0300

    Get-PogManifestHash: Show if the retrieved hash matches the specified hash, if available

--- app/Pog/container/Install/Env_GetInstallHash.psm1
+++ app/Pog/container/Install/Env_GetInstallHash.psm1
@@ -20,15 +20,28 @@ Export function __main {
         if ($Installer.Url -is [scriptblock]) {& $Installer.Url} else {$Installer.Url}
     }
 
+    $ExpectedHash = if ($Installer.ContainsKey("ExpectedHash")) {$Installer.ExpectedHash}
+        elseif ($Installer.ContainsKey("Hash")) {$Installer.Hash}
+        else {$null}
+
     $UserAgent = if ($Installer.ContainsKey("UserAgent")) {$Installer.UserAgent}
         else {[UserAgentType]::PowerShell}
 
     Write-Information "Retrieving the file hash for '$Url'..."
     $Hash = Get-UrlFileHash $Url -DownloadParams @{UserAgent = $UserAgent} -ShouldCache
+    $Hash | Set-Clipboard
+
     Write-Host ""
     Write-Host "Hash for the file at '$Url' (copied to clipboard):"
     Write-Host "$Hash"
-    $Hash | Set-Clipboard
+
+    if ($ExpectedHash) {
+        if ($ExpectedHash -eq $Hash) {
+            Write-Host -ForegroundColor Green "Matches the expected hash specified in the manifest."
+        } else {
+            throw "The retrieved hash does not match the expected hash specified in the manifest (expected: '$ExpectedHash')."
+        }
+    }
 }
 
 <# This function is called after __main finishes. #>
