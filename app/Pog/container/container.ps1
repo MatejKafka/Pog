fb28f85831d5c49c487f802c21be390c1fc25976
Matej Kafka <git@a-n.cc>
2022-07-02 04:40:03 +0300

    - Cleanup container.ps1

--- app/Pog/container/container.ps1
+++ app/Pog/container/container.ps1
@@ -44,12 +44,12 @@ switch ($ContainerType) {
 }
 
 # override Import-Module to hide the default verbose prints when -Verbose is set for the container environment
-$_OrigImport = Get-Command Import-Module
+# note that this only applies to custom imports inside the manifest script block, as all Pog modules are imported before this override
 function global:Import-Module {
 	# $VerbosePreference is set globally below, so we'd need to overwrite it, and then set it back,
 	#  as it interacts weirdly with -Verbose:$false, which apparently doesn't work here for some reason;
 	#  it seems as the cleanest solution to do `4>$null`, which just hides the Verbose stream alltogether
-	& $_OrigImport @Args 4>$null
+	Microsoft.PowerShell.Core\Import-Module @Args 4>$null
 }
 
 
@@ -72,7 +72,6 @@ Set-Variable -Scope Global -Option Constant -Name "_Pog" -Value @{
 # $this probably cannot be constant, as it would break internal behavior
 # it is used inside the manifest to refer to fields of the manifest itself to emulate class-like behavior
 Set-Variable -Name this -Value $Manifest
-Set-Variable -Name ManifestRoot -Value (Resolve-Path -Relative (Split-Path $ManifestPath))
 
 # cleanup variables
 Remove-Variable PackageName
