1ceefe1e4350a19fecfbdcdcb53e1af4f12605d7
Matej Kafka <git@a-n.cc>
2022-12-23 19:23:34 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,12 +1,13 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
-# this module only uses library functions, so it is safe to import even during setup
+# these modules only use library functions, so it is safe to import them even during setup
 Import-Module $PSScriptRoot/app/Pog/container/container_lib/Environment
+Import-Module $PSScriptRoot/app/Pog/lib/Utils
 
 
 function newdir($Dir) {
-    $Dir = Join-Path $PSScriptRoot $Dir
+    $Dir = Resolve-VirtualPath $PSScriptRoot/$Dir
     if (Test-Path -PathType Container $Dir) {
         return
     }
@@ -14,17 +15,17 @@ function newdir($Dir) {
         # exists, but not a directory
         $null = rm -Recurse $Dir
     }
+    Write-Host "Creating directory '$Dir'."
     $null = New-Item -Type Directory -Force $Dir
 }
 
-echo "Setting up all directories required by Pog..."
 
-# This duplicates the path definitions in app/Pog/Paths.psm1, but we cannot use those,
-#  because the file assumes all the paths already exist and fails otherwise
+Write-Host "Setting up all directories required by Pog..."
+
 newdir "./data"
 newdir "./cache"
 # local manifest repository
-newdir "./data/manifests"
+# ./data/manifests is created below
 newdir "./data/manifest_generators"
 # directory where commands are exported; is added to PATH
 newdir "./data/package_bin"
@@ -32,13 +33,31 @@ newdir "./data/package_bin"
 newdir "./cache/download_cache"
 newdir "./cache/download_tmp"
 
-echo "Checking if package root '$(Resolve-Path .\..)' is registered..."
-$ROOT_FILE_PATH = Join-Path $PSScriptRoot "./data/roots.txt"
+$ROOT_FILE_PATH = Join-Path $PSScriptRoot "./data/package_roots.txt"
 if (-not (Test-Path -PathType Leaf $ROOT_FILE_PATH)) {
-    (Resolve-Path .\..) | Set-Content $ROOT_FILE_PATH
+    $DefaultContentRoot = Resolve-Path $PSScriptRoot\..
+    Write-Host "Registering a Pog package root at '$DefaultContentRoot'..."
+    Set-Content $ROOT_FILE_PATH -Value $DefaultContentRoot
 }
 
-echo "Setting up PATH and PSModulePath..."
+# TODO: when a move comprehensive support for remote repository is implemented, update this
+$MANIFEST_REPO_PATH = Resolve-VirtualPath "$PSScriptRoot/data/manifests"
+if (-not (Test-Path $MANIFEST_REPO_PATH)) {
+    # manifest repository is not initialized, download it
+    Write-Host "Downloading package manifests from 'https://github.com/MatejKafka/PogPackages'..."
+    $ExtractedPath = "${env:TEMP}\PogPackages-$(New-Guid)"
+    $ArchivePath = "${ExtractedPath}.zip"
+    try {
+        Invoke-WebRequest "https://github.com/MatejKafka/PogPackages/archive/refs/heads/main.zip" -OutFile $ArchivePath
+        Expand-Archive $ArchivePath -DestinationPath $ExtractedPath -Force
+        Move-Item $ExtractedPath\* $MANIFEST_REPO_PATH -Force
+        Write-Host "Downloaded '$(@(ls -Directory $MANIFEST_REPO_PATH).Count)' package manifests."
+    } finally {
+        Remove-Item -Force -ErrorAction Ignore $ArchivePath
+    }
+}
+
+Write-Host "Setting up PATH and PSModulePath..."
 # add Pog dir to PSModulePath
 Add-EnvPSModulePath (Resolve-Path "$PSScriptRoot\app")
 # add binary dir to PATH
@@ -47,7 +66,7 @@ Add-EnvPath -Prepend (Resolve-Path "$PSScriptRoot\data\package_bin")
 
 # ====================================================================================
 # now, we should be ready to import Pog
-echo "Importing Pog...`n"
+Write-Host "Importing Pog...`n"
 Import-Module Pog
 
 try {
@@ -55,15 +74,30 @@ try {
 } catch {
     throw ("Failed to enable the 7zip package, required for correct functioning of Pog. " + `
             "The 7zip package should be provided with Pog itself. Error: " + $_)
+    return
 }
 
-if (-not (Get-Command "7z" -ErrorAction Ignore)) {
-    throw "Setup of 7zip was successful, but we cannot find the 7z.exe binary that should be provided by 7zip."
+if (-not (Test-Path "$PSScriptRoot\data\package_bin\7z.exe")) {
+    throw "Setup of 7zip was successful, but we cannot find the 7z.exe binary that should be provided by the package."
+    return
 }
 
-echo ""
+try {
+    Enable-Pog OpenedFilesView
+} catch {
+    throw ("Failed to enable the OpenedFilesView package, required for correct functioning of Pog. " + `
+            "The OpenedFilesView package should be provided with Pog itself. Error: " + $_)
+    return
+}
+
+if (-not (Test-Path "$PSScriptRoot\data\package_bin\OpenedFilesView.exe")) {
+    throw "Setup of OpenedFilesView was successful, but we cannot find the OpenedFilesView.exe binary that should be provided by the package."
+    return
+}
+
+Write-Host ""
 
 # now, everything should be setup correctly, enable Pog itself to validate (it doesn't do anything, just prints a success message)
 Enable-Pog Pog
 
-echo ""
+Write-Host ""
