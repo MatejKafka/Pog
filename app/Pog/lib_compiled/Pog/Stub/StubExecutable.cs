083e78f7ae864ef032b0f28223571d5fe2133508
Matej Kafka <git@a-n.cc>
2023-06-03 02:34:07 +0200

    lib_compiled: Simplify StubExecutable, add option to read resources from a separate file

--- app/Pog/lib_compiled/Pog/Stub/StubExecutable.cs
+++ app/Pog/lib_compiled/Pog/Stub/StubExecutable.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using JetBrains.Annotations;
+using Pog.Native;
 using Pog.Utils;
-using static Pog.Native;
 using ResourceType = Pog.Native.PeResources.ResourceType;
 
 namespace Pog.Stub;
@@ -19,7 +18,6 @@ namespace Pog.Stub;
 // 3) check if the configuration of a stub matches an expected one
 //    - encode stub data and compare with stored stub data
 //    - read target resources, compare with the hash stored in the stub
-[PublicAPI]
 public class StubExecutable {
     // stub data are stored as an RCDATA resource at index 1
     private static readonly PeResources.ResourceId StubDataResourceId = new(ResourceType.RcData, 1);
@@ -30,15 +28,15 @@ public class StubExecutable {
     private static readonly string[] SupportedTargetExtensions = {".exe", ".com", ".cmd", ".bat"};
 
     /// If true, argv[0] is always replaced with TargetPath, otherwise the path to the stub is kept as argv[0].
-    public bool ReplaceArgv0;
-    public string TargetPath;
-    public string? WorkingDirectory;
-    public string[]? Arguments;
-    public Dictionary<string, string>? EnvironmentVariables;
+    public readonly bool ReplaceArgv0;
+    public readonly string TargetPath;
+    public readonly string? WorkingDirectory;
+    public readonly string[]? Arguments;
+    public readonly Dictionary<string, string>? EnvironmentVariables;
 
     public StubExecutable(string targetPath, string? workingDirectory = null,
             string[]? arguments = null, Dictionary<string, string>? environmentVariables = null) {
-        var targetExtension = Path.GetExtension(targetPath);
+        var targetExtension = Path.GetExtension(targetPath).ToLower();
         if (!SupportedTargetExtensions.Contains(targetExtension)) {
             throw new UnsupportedStubTargetTypeException(
                     $"Stub target '{targetPath}' has an unsupported extension. Supported extensions are: " +
@@ -60,55 +58,44 @@ public class StubExecutable {
 
     /// Ensures that the stub at stubPath is up-to-date.
     /// <returns>true if anything changed, false if stub is up-to-date</returns>
-    public bool UpdateStub(string stubPath) {
-        return IsTargetPeBinary() ? UpdateStubExe(stubPath) : UpdateStubOther(stubPath);
+    public bool UpdateStub(string stubPath, string? resourceSrcPath = null) {
+        return IsTargetPeBinary() ? UpdateStubExe(stubPath, resourceSrcPath) : UpdateStubOther(stubPath, resourceSrcPath);
     }
 
-    /// Updater method for targets that are not PE binaries, and don't have a subsystem and resources.
-    private bool UpdateStubOther(string stubPath) {
-        var stubData = StubDataEncoder.EncodeStub(this);
-
-        // assume a console subsystem
-        var originalSubsystem = PeSubsystem.SetSubsystem(stubPath, PeSubsystem.WindowsSubsystem.WindowsCui);
-        var subsystemChanged = originalSubsystem != PeSubsystem.WindowsSubsystem.WindowsCui;
-
-        // updater is somewhat slow, only instantiate it if necessary
-        using var stubUpdater = new LazyDisposable<PeResources.ResourceUpdater>(
-                () => new PeResources.ResourceUpdater(stubPath));
-
-        // `stub` must be closed before `.CommitChanges()` is called
-        using (var stub = new PeResources.Module(stubPath)) {
-            if (!IsStubDataUpToDate(stub, stubData)) {
-                stubUpdater.Value.SetResource(StubDataResourceId, stubData);
-            }
-
-            // delete any previously copied resources
-            foreach (var resourceType in CopiedResourceTypes) {
-                RemoveResources(stubUpdater, stub, resourceType);
-            }
-        }
-
-        // write the changes
-        if (stubUpdater.IsValueCreated) {
-            stubUpdater.Value.CommitChanges();
-        }
-
-        return subsystemChanged || stubUpdater.IsValueCreated;
-    }
-
-    private bool UpdateStubExe(string stubPath) {
-        var stubData = StubDataEncoder.EncodeStub(this);
-
+    private bool UpdateStubExe(string stubPath, string? resourceSrcPath) {
         // copy subsystem from the target binary
         var targetSubsystem = PeSubsystem.GetSubsystem(TargetPath);
         var originalSubsystem = PeSubsystem.SetSubsystem(stubPath, targetSubsystem);
         var subsystemChanged = targetSubsystem != originalSubsystem;
 
-        // open stub and target
-        using var target = new PeResources.Module(TargetPath);
+        // copy resources from either target, or a separate module
+        var updatedResources = UpdateStubResources(stubPath, resourceSrcPath ?? TargetPath);
+
+        return subsystemChanged || updatedResources;
+    }
+
+    /// Updater method for targets that are not PE binaries, and don't have a subsystem and resources.
+    private bool UpdateStubOther(string stubPath, string? resourceSrcPath) {
+        // assume a console subsystem
+        var originalSubsystem = PeSubsystem.SetSubsystem(stubPath, PeSubsystem.WindowsSubsystem.WindowsCui);
+        var subsystemChanged = originalSubsystem != PeSubsystem.WindowsSubsystem.WindowsCui;
+
+        // target does not have any resources, since it's not a PE binary; either copy resources
+        //  from a separate module, or delete any existing resources, if any
+        var updatedResources = UpdateStubResources(stubPath, resourceSrcPath);
+
+        return subsystemChanged || updatedResources;
+    }
+
+    private bool UpdateStubResources(string stubPath, string? resourceSrcPath) {
+        var stubData = StubDataEncoder.EncodeStub(this);
+
         // updater is somewhat slow, only instantiate it if necessary
         using var stubUpdater = new LazyDisposable<PeResources.ResourceUpdater>(
                 () => new PeResources.ResourceUpdater(stubPath));
+
+        // open the module we copy resources from, if any
+        using var resourceSrc = resourceSrcPath == null ? null : new PeResources.Module(resourceSrcPath);
 
         // `stub` must be closed before `.CommitChanges()` is called
         using (var stub = new PeResources.Module(stubPath)) {
@@ -116,9 +103,14 @@ public class StubExecutable {
                 stubUpdater.Value.SetResource(StubDataResourceId, stubData);
             }
 
-            // ensure copied resources are up-to-date with target
             foreach (var resourceType in CopiedResourceTypes) {
-                UpdateResources(stubUpdater, stub, target, resourceType);
+                if (resourceSrc != null) {
+                    // ensure copied resources are up-to-date with target
+                    UpdateResources(stubUpdater, stub, resourceSrc, resourceType);
+                } else {
+                    // delete any previously copied resources
+                    RemoveResources(stubUpdater, stub, resourceType);
+                }
             }
         }
 
@@ -127,56 +119,50 @@ public class StubExecutable {
             stubUpdater.Value.CommitChanges();
         }
 
-        return subsystemChanged || stubUpdater.IsValueCreated;
+        return stubUpdater.IsValueCreated;
     }
 
-    private bool IsStubDataUpToDate(PeResources.Module stub, Span<byte> stubData) {
+    private static bool IsStubDataUpToDate(PeResources.Module stub, Span<byte> stubData) {
         return stub.TryGetResource(StubDataResourceId, out var currentStubData) && stubData.SequenceEqual(currentStubData);
+    }
+
+    private void WriteNewStubResources(string stubPath, string? resourceSrcPath) {
+        var stubData = StubDataEncoder.EncodeStub(this);
+        using var stubUpdater = new PeResources.ResourceUpdater(stubPath);
+
+        // write stub data
+        stubUpdater.SetResource(StubDataResourceId, stubData);
+
+        if (resourceSrcPath != null) {
+            using var target = new PeResources.Module(resourceSrcPath);
+
+            // copy resources from target
+            foreach (var resourceType in CopiedResourceTypes) {
+                CopyResources(stubUpdater, target, resourceType);
+            }
+        }
+
+        stubUpdater.CommitChanges();
     }
 
     /// Configures a new stub. The stub binary at `stubPath` should already exist.
     /// Assumes that the stub binary has no existing resources.
-    public void WriteNewStub(string stubPath) {
+    public void WriteNewStub(string stubPath, string? resourceSrcPath = null) {
         if (IsTargetPeBinary()) {
-            WriteNewStubExe(stubPath);
+            // copy subsystem from target binary
+            var targetSubsystem = PeSubsystem.GetSubsystem(TargetPath);
+            PeSubsystem.SetSubsystem(stubPath, targetSubsystem);
+
+            // write stub data and copy resources from either target, or a separate module
+            WriteNewStubResources(stubPath, resourceSrcPath ?? TargetPath);
         } else {
-            WriteNewStubOther(stubPath);
+            // write stub data, copy resources from the passed module, if any
+            WriteNewStubResources(stubPath, resourceSrcPath);
         }
-    }
-
-    public void WriteNewStubOther(string stubPath) {
-        var stubData = StubDataEncoder.EncodeStub(this);
-        using var stubUpdater = new PeResources.ResourceUpdater(stubPath);
-
-        // write stub data
-        stubUpdater.SetResource(StubDataResourceId, stubData);
-        stubUpdater.CommitChanges();
-    }
-
-    public void WriteNewStubExe(string stubPath) {
-        var stubData = StubDataEncoder.EncodeStub(this);
-
-        // copy subsystem from target binary
-        var targetSubsystem = PeSubsystem.GetSubsystem(TargetPath);
-        PeSubsystem.SetSubsystem(stubPath, targetSubsystem);
-
-        // open stub and target
-        using var target = new PeResources.Module(TargetPath);
-        using var stubUpdater = new PeResources.ResourceUpdater(stubPath);
-
-        // write stub data
-        stubUpdater.SetResource(StubDataResourceId, stubData);
-
-        // copy icons and version from target
-        foreach (var resourceType in CopiedResourceTypes) {
-            CopyResources(stubUpdater, target, resourceType);
-        }
-
-        stubUpdater.CommitChanges();
     }
 
     /// Copies resources of given `type`, assumes that the destination binary does not have any resources of type `type`.
-    private void CopyResources(PeResources.ResourceUpdater updater, PeResources.Module src, ResourceType type) {
+    private static void CopyResources(PeResources.ResourceUpdater updater, PeResources.Module src, ResourceType type) {
         try {
             src.IterateResourceNames(type, name => {
                 updater.CopyResourceFrom(src, type, name);
@@ -187,7 +173,7 @@ public class StubExecutable {
         }
     }
 
-    private void RemoveResources(Lazy<PeResources.ResourceUpdater> updater, PeResources.Module dest, ResourceType type) {
+    private static void RemoveResources(Lazy<PeResources.ResourceUpdater> updater, PeResources.Module dest, ResourceType type) {
         try {
             dest.IterateResourceNames(type, name => {
                 updater.Value.DeleteResource(type, name);
@@ -200,11 +186,11 @@ public class StubExecutable {
 
     /// Ensures that `dest` has the same resources of type `type` as `src`. Copies all missing resources, deletes any extra resources.
     /// Only instantiates `updater` if something needs to be copied.
-    private void UpdateResources(Lazy<PeResources.ResourceUpdater> updater, PeResources.Module dest, PeResources.Module src,
+    private static void UpdateResources(Lazy<PeResources.ResourceUpdater> updater, PeResources.Module dest, PeResources.Module src,
             ResourceType type) {
         // list resources of `type` in src
         if (!src.TryGetResourceNames(type, out var srcNames)) {
-            return;
+            srcNames = new();
         }
 
         // copy all non-matching resources
