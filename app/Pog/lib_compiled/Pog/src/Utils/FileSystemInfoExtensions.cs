43c7790bee552b2e9a98cdbe54d4913d1f7f43c9
Matej Kafka <git@a-n.cc>
2023-08-11 21:04:07 +1000

    -

---
+++ app/Pog/lib_compiled/Pog/src/Utils/FileSystemInfoExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace Pog.Utils;
+
+public static class FileSystemInfoExtensions {
+    public static string GetBaseName(this FileSystemInfo info) {
+        return Path.GetFileNameWithoutExtension(info.Name);
+    }
+}
