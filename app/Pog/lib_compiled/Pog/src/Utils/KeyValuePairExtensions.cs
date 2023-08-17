c5c3e5ee14d852ba275f60b1218f5512e29949f5
Matej Kafka <git@a-n.cc>
2023-08-17 16:35:00 +1000

    -

---
+++ app/Pog/lib_compiled/Pog/src/Utils/KeyValuePairExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Pog.Utils;
+
+public static class KeyValuePairExtensions {
+    public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> keyValuePair,
+            out TKey key, out TValue value) {
+        key = keyValuePair.Key;
+        value = keyValuePair.Value;
+    }
+}
