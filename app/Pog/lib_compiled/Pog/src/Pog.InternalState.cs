3a1c76c29ce3abc92efe4a13437845e6a4c95e1f
Matej Kafka <git@a-n.cc>
2023-08-17 16:33:49 +1000

    Pog.InternalState: Fix root directory lookup for the debug build
    Finding `lib_compiled` in the library path instead of assuming the .dll is in the `lib_compiled` directory works correctly with the debug build, which is kept in a subdirectory.

--- app/Pog/lib_compiled/Pog/src/Pog.InternalState.cs
+++ app/Pog/lib_compiled/Pog/src/Pog.InternalState.cs
@@ -1,13 +1,32 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Reflection;
-using JetBrains.Annotations;
 
 namespace Pog;
 
-[PublicAPI]
 public static class InternalState {
-    public static readonly string RootDirPath = Path.GetFullPath(Path.Combine(
-            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, @"..\..\..")); // app/Pog/lib_compiled/Pog.dll
+    private static string GetRootDirPath() {
+        const string libDirName = @"\lib_compiled\";
+        var assemblyPath = Assembly.GetExecutingAssembly().Location!;
+        var i = assemblyPath.LastIndexOf(libDirName, StringComparison.InvariantCultureIgnoreCase);
+        if (i < 0) {
+            throw new Exception("Could not find the root module directory, cannot initialize Pog.dll.");
+        }
+        var pogModuleDir = assemblyPath.Substring(0,i);
+        return Path.GetFullPath(Path.Combine(pogModuleDir, @"..\..")); // app/Pog/lib_compiled
+    }
+
+    private static string? _rootDirPath;
+    public static string RootDirPath {
+        get => _rootDirPath ??= GetRootDirPath();
+        set {
+            if (_rootDirPath != null) {
+                throw new Exception("Cannot configure the Pog root directory, since there is already a previous " +
+                                    "configured path. The path must be set before any Pog modules are used.");
+            }
+            _rootDirPath = value;
+        }
+    }
 
     private static PathConfig? _pathConfig;
     public static PathConfig PathConfig => _pathConfig ??= new PathConfig(RootDirPath);
