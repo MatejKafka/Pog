94060cc74174b4cf8169bd0a8a2b0b453ee9ec8f
Matej Kafka <git@a-n.cc>
2023-08-18 14:29:54 +1000

    bugfix: Polyfills: Use correct `StringComparison` for System.IO.Path

--- app/Pog/lib_compiled/Polyfills/src/System.IO/PathInternal.Windows.cs
+++ app/Pog/lib_compiled/Polyfills/src/System.IO/PathInternal.Windows.cs
@@ -9,7 +9,7 @@ namespace Polyfills.System.IO
 {
     /// <summary>Contains internal path helpers that are shared between many projects.</summary>
     internal static partial class PathInternal {
-        internal static StringComparison StringComparison = StringComparison.InvariantCultureIgnoreCase;
+        internal static readonly StringComparison StringComparison = StringComparison.OrdinalIgnoreCase;
 
         // All paths in Win32 ultimately end up becoming a path to a File object in the Windows object manager. Passed in paths get mapped through
         // DosDevice symbolic links in the object tree to actual File objects under \Devices. To illustrate, this is what happens with a typical
