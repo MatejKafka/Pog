ecdd3c835d2a5fa28e08374f622159c6be56e5d0
Matej Kafka <git@a-n.cc>
2023-09-12 21:51:14 +1000

    -

--- app/Pog/_scripts/convert direct package to templated.ps1
+++ app/Pog/_scripts/convert direct package to templated.ps1
@@ -1,12 +1,24 @@
+param(
+        [Parameter(Mandatory)]
+        [string]
+    $PackageName
+)
+
 Import-Module Pog
 
-# 1) cd to the package directory in the repo
-# 2) create the .template manifest
-# 3) run this script
-ls */pog.psd1 -Exclude .template `
+# 1) create the .template manifest
+# 2) run this script
+$PackagePath = [Pog.InternalState]::Repository.GetPackage($PackageName, $true, $true).Path
+
+if (-not (Test-Path "$PackagePath/.template")) {
+    throw "Missing .template dir"
+}
+
+ls $PackagePath/*/pog.psd1 -Exclude .template `
     | % {[Pog.PackageManifest]::new($_)} `
     | % {[ordered]@{Version = $_.Version.ToString(); Hash = $_.Install[0].ExpectedHash}} `
     | % {
-        [Pog.ManifestTemplateFile]::SerializeSubstitutionFile((Resolve-VirtualPath "$($_.Version).psd1"), $_)
-        rm -Recurse $_.Version
+        $ManifestPath = Resolve-VirtualPath "$PackagePath/$($_.Version).psd1"
+        [Pog.ManifestTemplateFile]::SerializeSubstitutionFile($ManifestPath, $_)
+        rm -Recurse "$PackagePath/$($_.Version)"
     }
