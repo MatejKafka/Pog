b6aab4dff5b0caa76359fae5c3b0b24fad943d9f
Matej Kafka <git@a-n.cc>
2024-03-16 00:38:37 +0100

    -

--- app/Pog/_scripts/add an icon to a package directory using Desktop.ini.ps1
+++ app/Pog/_scripts/add an icon to a package directory using Desktop.ini.ps1
@@ -1,26 +1,45 @@
+param(
+		[string[]]
+	$PackageName = $null
+)
+
 function addIcon($Dir) {
-    $Dir = Resolve-Path $Dir
-    $Shortcuts = ls $Dir -Filter *.lnk
-    if (@($Shortcuts).Count -eq 0) {
-        throw "No shortcuts."
-    } elseif (@($Shortcuts).Count -gt 1) {
-        throw "Multiple shortcuts."
+    $Dir = Get-Item $Dir
+
+    $Shortcut = if (Test-Path "$Dir\$($Dir.Name).lnk") {
+        Get-Item "$Dir\$($Dir.Name).lnk"
+    } else {
+        ls $Dir -Filter *.lnk
     }
 
-    $s = Get-Shortcut $Shortcuts[0]
+    if (@($Shortcut).Count -eq 0) {
+        throw "No shortcuts."
+    } elseif (@($Shortcut).Count -gt 1) {
+		throw "Multiple shortcuts."
+    }
+
+    $s = Get-Shortcut $Shortcut
     $i = $s.IconLocation.LastIndexOf(",")
     $path = $s.IconLocation.Substring(0, $i)
     $index = $s.IconLocation.Substring($i + 1)
 
-    $DesktopIniFile = New-Item "$Dir\Desktop.ini" -Force -Value @"
+    Set-Content "$Dir\Desktop.ini" -Force -Value @"
 [.ShellClassInfo]
 ConfirmFileOp=0
 IconFile=$([System.IO.Path]::GetRelativePath($Dir, $path))
 IconIndex=$index
 "@
+
+	$DesktopIniFile = Get-Item "$Dir\Desktop.ini" -Force
     # make Desktop.ini hidden
     $DesktopIniFile.Attributes = $DesktopIniFile.Attributes -bor [System.IO.FileAttributes]::Hidden
-    # make the package directory read-only (otherwise, explorer.exe would ignore Desktop.ini
-    $d = Get-Item $Dir
-    $d.Attributes = $d.Attributes -bor [System.IO.FileAttributes]::ReadOnly
+    # make the package directory read-only (otherwise, explorer.exe would ignore Desktop.ini)
+    $Dir.Attributes = $Dir.Attributes -bor [System.IO.FileAttributes]::ReadOnly
+}
+
+
+Import-Module Pog
+foreach ($p in Get-PogPackage $PackageName) {
+	try {addIcon $p.Path}
+	catch {Write-Warning ($p.PackageName + ": $_")}
 }
