0021006dfa113523c045c358e90132944bdc700d
Matej Kafka <git@a-n.cc>
2024-04-19 02:20:40 +0200

    Add Get-GitHubRelease cmdlet accessible to manifest generators

--- app/Pog/Pog.psm1
+++ app/Pog/Pog.psm1
@@ -218,11 +218,51 @@ function UpdateSinglePackage([string]$PackageName, [string[]]$Version,  [switch]
 	}
 }
 
+# used from inside the manifest generators
+function Get-GitHubRelease {
+	[CmdletBinding()]
+	param(
+			[Parameter(Mandatory, ValueFromPipeline)]
+			[ValidatePattern("^[^/\s]+/[^/\s]+$")]
+			[string[]]
+		$Repository,
+			### Retrieves tags instead of releases.
+			[switch]
+		$Tags
+	)
+
+	process {
+		foreach ($r in $Repository) {
+			$Endpoint = if ($Tags) {"tags"} else {"releases"}
+			$Url = "https://api.github.com/repos/$r/$Endpoint"
+			Write-Verbose "Listing GitHub releases for '$r'... (URL: $Url)"
+
+			try {
+				# piping through Write-Output enumerates the array returned by irm into individual values
+				#  (see https://github.com/PowerShell/PowerShell/issues/15280)
+				# -FollowRelLink automatically goes through all pages to get older releases
+				Invoke-RestMethod -UseBasicParsing -FollowRelLink $Url | Write-Output
+			} catch [Microsoft.PowerShell.Commands.HttpResponseException] {
+				$e = $_.Exception
+				if ($e.StatusCode -eq 404) {
+					throw "Cannot list $Endpoint for '$r', GitHub repository does not exist."
+				} elseif ($e.StatusCode -eq 403 -and $e.Response.ReasonPhrase -eq "rate limit exceeded") {
+					$Limit = try {$e.Response.Headers.GetValues("X-RateLimit-Limit")} catch {}
+					$LimitMsg = if ($Limit) {" (at most $Limit requests/hour are allowed)"}
+					throw "Cannot list $Endpoint for '$r', GitHub API rate limit exceeded$LimitMsg."
+				} else {
+					throw
+				}
+			}
+		}
+	}
+}
+
 # TODO: run this inside a container
 #  run both scriptblocks in the same container instance
 #  think what utility cmdlets can Pog reasonably provide and which is best left to existing tools like iwr/import
 #    likely provide cmdlets to work with github (listing releases, using regex to select asset URL) and calculate hash (Show-PogManifestHash)
-#  add Get-UrlHash in the container env
+#  add Get-UrlHash and Get-GitHubRelease in the container env; maybe also set $PSDefaultParameterValues to pass -UseBasicParsing to iwr and irm
 # FIXME: if -Force is passed, track if there are any leftover manifests (for removed versions) and delete them
 Export function Update-PogManifest {
 	# .SYNOPSIS
--- app/Pog/lib/Utils.psm1
+++ app/Pog/lib/Utils.psm1
@@ -8,5 +8,11 @@ Export function Resolve-VirtualPath {
 
 Export function Invoke-DollarUnder {
 	param([Parameter(Mandatory)][scriptblock]$Sb, $DollarUnder, [Parameter(ValueFromRemainingArguments)][object[]]$ExtraArgs)
-	return $Sb.InvokeWithContext($null, [psvariable[]]@([psvariable]::new("_", $DollarUnder)), $ExtraArgs)
+
+	try {
+		return $Sb.InvokeWithContext($null, [psvariable[]]@([psvariable]::new("_", $DollarUnder)), $ExtraArgs)
+	} catch [System.Management.Automation.MethodInvocationException] {
+		# rethrow inner exception
+		throw $_.Exception.InnerException
+	}
 }
