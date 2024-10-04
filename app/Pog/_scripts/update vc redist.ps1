f513f58e447e7838bb86930f44348dde431392d0
Matej Kafka <git@a-n.cc>
2024-10-04 15:50:31 +0200

    -

--- app/Pog/_scripts/update vc redist.ps1
+++ app/Pog/_scripts/update vc redist.ps1
@@ -1,5 +1,14 @@
-$SrcDir = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -find VC/Redist/MSVC/*/x64
+$SrcDir = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -find VC/Redist/MSVC/*/x64 -products *
 if ($null -eq $SrcDir) {
     throw "Could not find VC Redistributable."
 }
-ls -Recurse -File $SrcDir | % {Write-Host $_; $_} | cp -Destination $PSScriptRoot\..\lib_compiled\vc_redist\
+
+$Existing = [System.Collections.Generic.HashSet[string]](ls $PSScriptRoot\..\lib_compiled\vc_redist | % Name)
+$null = $Existing.Remove("README.txt")
+ls -Recurse -File $SrcDir `
+    | % {$null = $Existing.Remove($_.Name); Write-Host $_; $_} `
+    | cp -Destination $PSScriptRoot\..\lib_compiled\vc_redist\
+
+$Existing | % {
+    Write-Host "Not updated: $_"
+}
