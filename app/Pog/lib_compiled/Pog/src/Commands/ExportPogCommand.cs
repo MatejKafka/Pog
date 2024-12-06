70e0db9c64bd44c1b26e9cf1fe6d91380d5099e1
Matej Kafka <git@a-n.cc>
2024-12-07 00:46:52 +0100

    Export-Pog: Fix handling of name casing changes
    Make name comparisons case-insensitive.

--- app/Pog/lib_compiled/Pog/src/Commands/ExportPogCommand.cs
+++ app/Pog/lib_compiled/Pog/src/Commands/ExportPogCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Management.Automation;
@@ -34,6 +35,7 @@ public sealed class ExportPogCommand() : ImportedPackageCommand(false) {
                     WriteVerbose($"Shortcut '{shortcutName}' is already exported from this package.");
                     continue;
                 } else {
+                    // TODO: detect if the shortcut is from this package and do not print this (to match exported commands)
                     WriteWarning($"Overwriting existing shortcut '{shortcutName}'...");
                 }
             }
@@ -43,7 +45,17 @@ public sealed class ExportPogCommand() : ImportedPackageCommand(false) {
         }
     }
 
+    private static IEnumerable<string> EnumerateMatchingCommands(string dirPath, string cmdName) {
+        // filter out files with a dot before the extension (e.g. `arm-none-eabi-ld.bfd.exe`)
+        return Directory.EnumerateFiles(dirPath, $"{cmdName}.*").Where(cmdPath => {
+            return string.Equals(Path.GetFileNameWithoutExtension(cmdPath), cmdName, StringComparison.OrdinalIgnoreCase);
+        });
+    }
+
     private void ExportCommands(ImportedPackage p) {
+        // ensure the export dir exists
+        Directory.CreateDirectory(InternalState.PathConfig.ExportedCommandDir);
+
         foreach (var command in p.EnumerateExportedCommands()) {
             var cmdName = command.GetBaseName();
             var targetPath = Path.Combine(InternalState.PathConfig.ExportedCommandDir, command.Name);
@@ -53,19 +65,16 @@ public sealed class ExportPogCommand() : ImportedPackageCommand(false) {
                 continue;
             }
 
-            var matchingCommands = Directory
-                    .EnumerateFiles(InternalState.PathConfig.ExportedCommandDir, $"{cmdName}.*")
-                    // filter out files with a dot before the extension (e.g. `arm-none-eabi-ld.bfd.exe`)
-                    .Where(cmdPath => Path.GetFileNameWithoutExtension(cmdPath) == cmdName)
-                    .ToArray();
-
+            var matchingCommands = EnumerateMatchingCommands(InternalState.PathConfig.ExportedCommandDir, cmdName).ToArray();
             if (matchingCommands.Length != 0) {
                 if (matchingCommands.Length > 1) {
                     WriteWarning("Pog developers fucked something up, and there are multiple colliding commands. " +
                                  "Plz send bug report.");
                 }
                 WriteWarning($"Overwriting existing command '{cmdName}'...");
-                foreach (var collidingCmdPath in matchingCommands) File.Delete(collidingCmdPath);
+                foreach (var collidingCmdPath in matchingCommands) {
+                    File.Delete(collidingCmdPath);
+                }
             }
 
             try {
