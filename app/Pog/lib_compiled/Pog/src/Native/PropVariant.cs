02b2b11c053721e9aab8f98486b250cf72e08c62
Matej Kafka <git@a-n.cc>
2025-01-13 02:30:31 +0100

    Native.Shortcut: Add support for custom string properties

---
+++ app/Pog/lib_compiled/Pog/src/Native/PropVariant.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.System.Com.StructuredStorage;
+using Windows.Win32.System.Variant;
+using Windows.Win32.UI.Shell.PropertiesSystem;
+
+namespace Pog.Native;
+
+// the CsWin32-generated fields are pretty ugly, but still easier than defining it manually
+internal struct PropVariant : IDisposable {
+    private PROPVARIANT _variant = new();
+
+    public PROPVARIANT Variant => _variant;
+    public VARENUM Type {
+        get => _variant.Anonymous.Anonymous.vt;
+        private set => _variant.Anonymous.Anonymous.vt = value;
+    }
+
+    internal PropVariant(PROPVARIANT variant) {
+        _variant = variant;
+    }
+
+    public unsafe PropVariant(string value) {
+        Type = VARENUM.VT_LPWSTR;
+        _variant.Anonymous.Anonymous.Anonymous.pwszVal = (char*) Marshal.StringToCoTaskMemUni(value).ToPointer();
+    }
+
+    public bool TryGetString(out string value) {
+        if (Type != VARENUM.VT_LPWSTR) {
+            value = null!;
+            return false;
+        }
+        value = _variant.Anonymous.Anonymous.Anonymous.pwszVal.ToString();
+        return true;
+    }
+
+    public void Dispose() {
+        var hr = PInvoke.PropVariantClear(ref _variant);
+        Marshal.ThrowExceptionForHR(hr);
+    }
+}
+
+internal static class PropVariantExtensions {
+    public static uint GetCount(this IPropertyStore store) {
+        store.GetCount(out var count);
+        return count;
+    }
+
+    public static unsafe (Guid, uint) GetKeyAt(this IPropertyStore store, uint index) {
+        var key = new PROPERTYKEY();
+        store.GetAt(index, &key);
+        return (key.fmtid, key.pid);
+    }
+
+    public static unsafe PropVariant GetValue(this IPropertyStore store, Guid formatId, uint propertyId) {
+        var key = new PROPERTYKEY {fmtid = formatId, pid = propertyId};
+        store.GetValue(&key, out var variant);
+        return new(variant);
+    }
+
+    public static unsafe void SetValue(this IPropertyStore store, Guid formatId, uint propertyId, PropVariant value) {
+        var key = new PROPERTYKEY {fmtid = formatId, pid = propertyId};
+        store.SetValue(&key, value.Variant);
+    }
+
+    public static IEnumerable<(Guid, uint)> EnumerateKeys(this IPropertyStore store) {
+        store.GetCount(out var propCount);
+        for (uint i = 0; i < propCount; i++) {
+            yield return store.GetKeyAt(i);
+        }
+    }
+
+    public static IEnumerable<KeyValuePair<(Guid, uint), PropVariant>> GetEnumerator(this IPropertyStore store) {
+        foreach (var key in store.EnumerateKeys()) {
+            yield return new(key, store.GetValue(key.Item1, key.Item2));
+        }
+    }
+}
--- app/Pog/lib_compiled/Pog/src/Native/Shortcut.cs
+++ app/Pog/lib_compiled/Pog/src/Native/Shortcut.cs
@@ -6,6 +6,7 @@ using Windows.Win32.Foundation;
 using Windows.Win32.System.Com;
 using Windows.Win32.UI.Shell;
 using Windows.Win32.UI.Shell.Common;
+using Windows.Win32.UI.Shell.PropertiesSystem;
 
 namespace Pog.Native;
 
@@ -17,6 +18,7 @@ internal class Shortcut {
     private readonly ShellLink _shellLinkInstance = new();
     private readonly IShellLinkW _shellLink;
     private readonly IPersistFile _persistFile;
+    private readonly IPropertyStore _propertyStore;
 
     /// Indicates if this Shortcut instance was loaded from an existing shortcut file.
     public bool Loaded {get; private set;} = false;
@@ -72,6 +74,8 @@ internal class Shortcut {
         _shellLink = (IShellLinkW) _shellLinkInstance;
         // ReSharper disable once SuspiciousTypeConversion.Global
         _persistFile = (IPersistFile) _shellLinkInstance;
+        // ReSharper disable once SuspiciousTypeConversion.Global
+        _propertyStore = (IPropertyStore) _shellLinkInstance;
     }
 
     public Shortcut(string srcPath) : this() {
@@ -79,12 +83,26 @@ internal class Shortcut {
     }
 
     public void LoadFrom(string path) {
-        _persistFile.Load(path, (int) STGM.STGM_READ);
+        // we need READWRITE, otherwise setting values in IPropertyStore fails (but no write is done anyway)
+        _persistFile.Load(path, (int) STGM.STGM_READWRITE);
         Loaded = true;
     }
 
     public void SaveTo(string path) {
         _persistFile.Save(path, false);
+    }
+
+    public bool TryGetStringProperty(Guid formatId, uint propertyId, out string str) {
+        using var variant = _propertyStore.GetValue(formatId, propertyId);
+        return variant.TryGetString(out str);
+    }
+
+    /// <param name="formatId">Format (top-level group, can be a project identifier) GUID.</param>
+    /// <param name="propertyId">Property ID inside the format group.</param>
+    /// <param name="value">The string to store in the property.</param>
+    public void SetStringProperty(Guid formatId, uint propertyId, string value) {
+        // the temporary PropVariant is not disposed, the store takes ownership
+        _propertyStore.SetValue(formatId, propertyId, new(value));
     }
 
     private static unsafe void WriteString(string str, Action<PCWSTR> fn) {
