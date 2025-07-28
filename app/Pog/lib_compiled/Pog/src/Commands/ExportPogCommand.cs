6080b0667a0a41960b4a723fe26c76fc844da521
Matej Kafka <git@a-n.cc>
2025-07-28 03:30:15 +0200

    Export-Pog: Move implementation to a separate internal command

--- app/Pog/lib_compiled/Pog/src/Commands/ExportPogCommand.cs
+++ app/Pog/lib_compiled/Pog/src/Commands/ExportPogCommand.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Management.Automation;
+﻿using System.Management.Automation;
 using JetBrains.Annotations;
 using Pog.Commands.Common;
-using Pog.Utils;
+using Pog.InnerCommands;
 
 namespace Pog.Commands;
+
+// TODO: it probably makes sense to keep Enable-Pog and Export-Pog separate, as you might want to invoke each without the other;
+//  however, we still want to have some form of automatic export in Enable-Pog; couldn't we use a heuristic like "if we're
+//  creating a new export, there's nothing under that name yet, and our package already had something exported, export it"?
 
 /// <summary>Exports shortcuts and commands from the package.</summary>
 /// <para>
@@ -16,78 +17,8 @@ namespace Pog.Commands;
 [Cmdlet(VerbsData.Export, "Pog", DefaultParameterSetName = DefaultPS, SupportsShouldProcess = true)]
 public sealed class ExportPogCommand() : ImportedPackageCommand(false) {
     protected override void ProcessPackage(ImportedPackage package) {
-        ExportShortcuts(package);
-        ExportCommands(package);
+        InvokePogCommand(new ExportPog(this) {
+            Package = package,
+        });
     }
-
-    private void ExportShortcuts(ImportedPackage p) {
-        foreach (var shortcut in p.EnumerateExportedShortcuts()) {
-            var shortcutName = shortcut.GetBaseName();
-            var target = GloballyExportedShortcut.FromLocal(shortcut.FullName);
-
-            if (target.IsFromPackage(p)) {
-                if (!target.UpdateFrom(shortcut)) {
-                    WriteVerbose($"Shortcut '{shortcutName}' is already exported from this package.");
-                    continue;
-                }
-            } else {
-                var ownerPath = target.SourcePackagePath;
-                if (target.OverwriteWith(shortcut)) {
-                    WriteWarning($"Overwritten an existing shortcut '{shortcutName}' from package " +
-                                 $"'{Path.GetFileName(ownerPath)}'.");
-                } else {
-                    // created new shortcut
-                }
-            }
-
-            WriteInformation($"Exported shortcut '{shortcutName}' from '{p.PackageName}'.");
-        }
-    }
-
-    private void ExportCommands(ImportedPackage p) {
-        // ensure the export dir exists
-        Directory.CreateDirectory(InternalState.PathConfig.ExportedCommandDir);
-
-        foreach (var command in p.EnumerateExportedCommands()) {
-            var cmdName = command.GetBaseName();
-            var target = GloballyExportedCommand.FromLocal(command.FullName);
-
-            if (command.FullName == target.Target) {
-                WriteVerbose($"Command '{cmdName}' is already globally exported from this package.");
-                continue;
-            }
-
-            var matchingCommands = target.EnumerateConflictingCommands().ToArray();
-            if (matchingCommands.Length != 0) {
-                WriteDebug($"Found {matchingCommands.Length} conflicting commands: {matchingCommands}");
-
-                if (matchingCommands.Length > 1) {
-                    WriteWarning($"Pog developers fucked something up, and there are multiple colliding commands for " +
-                                 $"'{cmdName}', please send a bug report.");
-                }
-
-                var ownerPath = matchingCommands[0].SourcePackagePath;
-                foreach (var collidingCmdPath in matchingCommands) {
-                    collidingCmdPath.Delete();
-                }
-
-                WriteWarning($"Overwritten an existing command '{cmdName}' from package '{Path.GetFileName(ownerPath)}'.");
-            }
-
-            try {
-                target.UpdateFrom(command);
-            } catch (Exception e) {
-                var category = e is UnauthorizedAccessException
-                        ? ErrorCategory.PermissionDenied
-                        : ErrorCategory.NotSpecified;
-                ThrowTerminatingError(new CommandExportException(
-                                $"Could not export command '{cmdName}' from '{p.PackageName}', symbolic link creation " +
-                                $"failed: {e.Message}", e),
-                        "CommandExportFailed", category, cmdName);
-            }
-            WriteInformation($"Exported command '{cmdName}' from '{p.PackageName}'.", null);
-        }
-    }
-
-    public class CommandExportException(string message, Exception innerException) : Exception(message, innerException);
 }
---
+++ app/Pog/lib_compiled/Pog/src/InnerCommands/ExportPog.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using Pog.InnerCommands.Common;
+using Pog.Utils;
+
+namespace Pog.InnerCommands;
+
+public sealed class ExportPog(PogCmdlet cmdlet) : VoidCommand(cmdlet) {
+    [Parameter] public required ImportedPackage Package;
+
+    public override void Invoke() {
+        ExportShortcuts();
+        ExportCommands();
+    }
+
+    private void ExportShortcuts() {
+        foreach (var shortcut in Package.EnumerateExportedShortcuts()) {
+            var shortcutName = shortcut.GetBaseName();
+            var target = GloballyExportedShortcut.FromLocal(shortcut.FullName);
+
+            if (target.IsFromPackage(Package)) {
+                if (!target.UpdateFrom(shortcut)) {
+                    WriteVerbose($"Shortcut '{shortcutName}' is already exported from this package.");
+                    continue;
+                }
+            } else {
+                var ownerPath = target.SourcePackagePath;
+                if (target.OverwriteWith(shortcut)) {
+                    WriteWarning($"Overwritten an existing shortcut '{shortcutName}' from package " +
+                                 $"'{Path.GetFileName(ownerPath)}'.");
+                } else {
+                    // created new shortcut
+                }
+            }
+
+            WriteInformation($"Exported shortcut '{shortcutName}' from '{Package.PackageName}'.");
+        }
+    }
+
+    private void ExportCommands() {
+        // ensure the export dir exists
+        Directory.CreateDirectory(InternalState.PathConfig.ExportedCommandDir);
+
+        foreach (var command in Package.EnumerateExportedCommands()) {
+            var cmdName = command.GetBaseName();
+            var target = GloballyExportedCommand.FromLocal(command.FullName);
+
+            if (command.FullName == target.Target) {
+                WriteVerbose($"Command '{cmdName}' is already globally exported from this package.");
+                continue;
+            }
+
+            var matchingCommands = target.EnumerateConflictingCommands().ToArray();
+            if (matchingCommands.Length != 0) {
+                WriteDebug($"Found {matchingCommands.Length} conflicting commands: {matchingCommands}");
+
+                if (matchingCommands.Length > 1) {
+                    WriteWarning($"Pog developers fucked something up, and there are multiple colliding commands for " +
+                                 $"'{cmdName}', please send a bug report.");
+                }
+
+                var ownerPath = matchingCommands[0].SourcePackagePath;
+                foreach (var collidingCmdPath in matchingCommands) {
+                    collidingCmdPath.Delete();
+                }
+
+                WriteWarning($"Overwritten an existing command '{cmdName}' from package '{Path.GetFileName(ownerPath)}'.");
+            }
+
+            try {
+                target.UpdateFrom(command);
+            } catch (Exception e) {
+                throw new CommandExportException($"Could not export command '{cmdName}' from '{Package.PackageName}', " +
+                                                 $"symbolic link creation failed: {e.Message}", e);
+            }
+            WriteInformation($"Exported command '{cmdName}' from '{Package.PackageName}'.");
+        }
+    }
+
+    public class CommandExportException(string message, Exception innerException) : Exception(message, innerException);
+}
